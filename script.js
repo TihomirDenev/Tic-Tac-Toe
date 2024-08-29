d48be0530efc906ab77bfc2c72adcbfbbf302b3f
Tihomir Denev <156659230+TihomirDenev@users.noreply.github.com>
2024-08-29 21:43:07 +0300

    Add event listener to restart button for game reset

--- script.js
+++ script.js
@@ -4,4 +4,8 @@ document.addEventListener('DOMContentLoaded', function () {
   const game = new GameEngine();
 
   game.startGame();
+
+  document.querySelector('.restart-button').addEventListener('click', () => {
+    game.restartGame();
+  });
 });
