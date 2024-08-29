0c056834ec456096cfa5f4dd242ed61ea0707e4c
Tihomir Denev <156659230+TihomirDenev@users.noreply.github.com>
2024-08-29 21:33:56 +0300

    Add constants for score IDs, messages and win combinations.

--- src/constants.js
+++ src/constants.js
@@ -7,3 +7,21 @@ export const GAME_BOARD_SIZE = 9;
 export const GAME_BOARD_ID = 'game-board';
 export const GAME_CELL_CLASS = '.game-cell';
 export const DATA_INDEX_ATTRIBUTE = 'data-index';
+export const SCORE_X_ID = 'score-x';
+export const SCORE_O_ID = 'score-o';
+export const PLAYER_X_ID = 'player-x';
+export const PLAYER_O_ID = 'player-o';
+export const TAKEN_CELL_MESSAGE = 'This cell is taken';
+export const TIE_MESSAGE = "It's a tie!";
+export const GAME_RESTARTED_MESSAGE = 'Game Restart!';
+
+export const WIN_COMBINATION = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
